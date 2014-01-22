b6cf1473b6ecf94eb3cf6d36e06b24aaa9a337ad
' <'>
2014-01-22 14:32:22 -0600

    exam

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Week20exam::Application.config.secret_key_base = '3208fc21835fdc141334b72b32bd28a8fb9c2da9ee0713de5a25d4694d590f09a5b4f009302260f9fc10cd93c2048e2159626accd837618ca05625b917e1ab91'
